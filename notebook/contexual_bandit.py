c5077b0df61358089996aae8cb11fce9f7e83fab
louis1022 <konuma1022@gmail.com>
2019-05-31 12:41:02 +0900

    20190531

--- notebook/contexual_bandit.py
+++ notebook/contexual_bandit.py
@@ -16,6 +16,11 @@ class OnePredictor():
         return np.c_[np.zeros((X.shape[0], 1)), np.ones((X.shape[0], 1))]
 
 
+class _BetaPredictor():
+    def predict_proba(self, X):
+        return np.c_[np.zeros((X.shape[0], 1)), np.ones((X.shape[0], 1))]
+
+
 class ContextualBandit():
 
     def __init__(self, base_model, n_arm,  n_estimator=100):
@@ -33,14 +38,16 @@ class ContextualBandit():
         self.estimators = self._reset_estimators()
 
         for arm_id in range(self.n_arm):
-            matched_X = X[(chosen_arm == arm_id)]
-            matched_y = y[(chosen_arm == arm_id)]
+            matched_X = X[chosen_arm == arm_id]
+            matched_y = y[chosen_arm == arm_id]
 
-            Parallel(n_jobs=-1, verbose=0, require="sharedmem")(
-                [delayed(self._fit_single)(arm_id, estimator_idx, matched_X, matched_y)
-                 for estimator_idx in range(self.n_estimator)])
+            # Parallel(n_jobs=-1, verbose=0, require="sharedmem")(
+            #     [delayed(self._fit_single)(arm_id, estimator_idx, matched_X, matched_y)
+            #      for estimator_idx in range(self.n_estimator)])
+            for estimator_idx in range(self.n_estimator):
+                self._fit_single(arm_id, estimator_idx, matched_X, matched_y)
 
-            self.smpl_count_each_arm[arm_id] += matched_X.shape[0]
+            self.smpl_count_each_arm[arm_id] += matched_y.shape[0]
 
     def _fit_single(self, arm_id, estimator_idx, X, y):
         _X, _y = self._bootstrapped_sampling(X, y)
@@ -54,31 +61,39 @@ class ContextualBandit():
 
     def _bootstrapped_sampling(self, X, y, sample_rate=0.8):
         data_size = X.shape[0]
-        bootstrapped_idx = np.random.randint(
-            0, data_size, int(data_size*sample_rate))
+        bootstrapped_idx = np.random.randint(0, data_size, int(data_size*sample_rate))
         return X[bootstrapped_idx], y[bootstrapped_idx]
 
     def _thompson_sampling(self, smpl):
         smpl_avg = np.mean(smpl, axis=0)
         smpl_std = np.std(smpl, axis=0)
-        return np.random.normal(smpl_avg, smpl_std, size=(1, len(smpl)))
+        return [np.random.normal(p, s) for p, s in zip(smpl_avg, smpl_std)]
 
     def _get_proba_with_thompson_sampling(self, arm_id, X):
 
         def _single_predict_proba(arm_id, estimator_idx, X):
             return self.estimators[arm_id, estimator_idx].predict_proba(X)[:, 1]
 
-        proba_result = Parallel(n_jobs=-1, verbose=0, require="sharedmem")(
-            [delayed(_single_predict_proba)(arm_id, estimator_idx, X)
-             for estimator_idx in range(self.n_estimator)])
+        # proba_result = Parallel(n_jobs=-1, verbose=0, require="sharedmem")(
+        #     [delayed(_single_predict_proba)(arm_id, estimator_idx, X)
+        #      for estimator_idx in range(self.n_estimator)])
+        proba_result = []
+        for estimator_idx in range(self.n_estimator):
+            proba_result.append(_single_predict_proba(arm_id, estimator_idx, X))
 
         proba_each_arm = self._thompson_sampling(proba_result)
         return proba_each_arm
+
+    def _get_proba_with_prior_distribution(self, smple):
+        return np.random.beta(5, 95, size=smple.shape[0])
 
     def predict(self, X):
         proba_each_users = np.zeros((self.n_arm, X.shape[0]))
 
         for arm_id in range(self.n_arm):
-            proba_each_users[arm_id, :] = self._get_proba_with_thompson_sampling(arm_id, X)
+            if self.smpl_count_each_arm[arm_id] < 1000:
+                proba_each_users[arm_id, :] = self._get_proba_with_prior_distribution(X)
+            else:
+                proba_each_users[arm_id, :] = self._get_proba_with_thompson_sampling(arm_id, X)
 
         return np.argmax(proba_each_users, axis=0)
