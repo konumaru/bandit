dcbc95080802ba08a29de377f7154191ba0f4215
louis1022 <konuma1022@gmail.com>
2019-06-20 03:23:09 +0900

    req.txt を自動で読み込み

--- setup.py
+++ setup.py
@@ -2,24 +2,32 @@
 
 # Learn more: https://github.com/kennethreitz/setup.py
 
+import os
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
 
+def strip_comments(l):
+    return l.split('#', 1)[0].strip()
+
+def reqs(*f):
+    return list(filter( None, [strip_comments(l) for l in open(os.path.join(os.getcwd(), *f)).readlines()] ))
+
 setup(
-    name='sample',
+    name='contextual_bandit',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='A library of contextual multi-arm bandits',
     long_description=readme,
     author='Kenneth Reitz',
     author_email='me@kennethreitz.com',
     url='https://github.com/kennethreitz/samplemod',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=reqs('requirements.txt')
 )
 
