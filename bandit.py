e01fe6ba512ed276517f6a85692e82820ec65673
louis1022 <konuma1022@gmail.com>
2019-07-08 03:14:36 +0900

    def bandit, epsilon greedy

--- bandit.py
+++ bandit.py
@@ -1,17 +1,33 @@
 import numpy as np
 
 
-class Bandit():
+class BaseBandit():
+
     def __init__(self, n_arm, context=None):
         self.n_arm = n_arm
         self.context = context
 
         self.avg_rewards = np.zeros(n_arm)
-        self.pulled_count = 0
         self.is_context = True if context else False
 
-    def update(self):
-        pass
 
-    def select_arm(self):
-        pass
+class EpsilonGreedy(BaseBandit):
+
+    def __init__(self, n_arm, context=None, epsilon=0.2):
+        super().__init__(n_arm, context=context)
+        self.epsilon = epsilon
+
+    def update(self, data):
+        self.avg_rewards = np.array([data[(data[:, 0] == arm_id), 1].mean()
+                                     for arm_id in range(self.n_arm)])
+
+    def select_arm(self, pull_count):
+        # 最適なアームを選択
+        optimal_arm_id = self.avg_rewards.argmax()
+        selected_arm = np.full(pull_count, fill_value=optimal_arm_id)
+
+        # 探索フラグを指定、探索アームを割り当て
+        explore_flag = np.random.rand(pull_count) < self.epsilon
+        explore_count = sum(explore_flag)
+        selected_arm[explore_flag] = np.random.choice(np.arange(self.n_arm), size=explore_count)
+        return selected_arm
--- main.py
+++ main.py
@@ -1,36 +1,71 @@
 import numpy as np
-from bandit import Bandit
+from bandit import EpsilonGreedy
+
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
 
 
-def generate_data(n_arm=5, n_samples=10000):
-    np.random.seed(n_arm)
+def generate_data(n_arm=5, n_samples=10000, random_state=42):
+    np.random.seed(random_state)
 
     n_noise = np.random.rand(n_samples)
-    weights = np.random.rand(n_arm) / 2
+    weights = np.random.rand(n_arm)
 
-    pulled_arm = np.random.randint(n_arm, size=n_samples)
-    rewards = (n_noise < weights[pulled_arm]).astype(np.int8)
+    pulled_arms = np.random.randint(n_arm, size=n_samples)
+    pulled_rewards = (n_noise < weights[pulled_arms]).astype(np.int8)
 
-    return pulled_arm, rewards, weights
+    data = np.c_[pulled_arms, pulled_rewards]
+
+    return data, weights
+
+
+def plot_result(result_dict):
+    plt.figure()
+    plt.xlabel('Select Arm Round')
+    plt.ylabel('Average Rewards')
+    plt.ylim(0.0, 1.0)
+    for key, val in result_dict.items():
+        plt.plot(val, label=key)
+    plt.legend()
+    plt.savefig('./images/main/result.png')
 
 
 def main():
-    bandit_model = Bandit(n_arm=3)
-
     n_arm = 5
-    n_samples = 10000
-    pulled_arm, rewards, weight = generate_data(n_arm, n_samples)
+    n_samples = 1000000
+    data, weight = generate_data(n_arm, n_samples)
 
-    print(weight)
+    print('Each Arm Weight Is ', weight)
+    print('Max Rewards Arm_id Is ', np.argmax(weight))
+    print('Max Average Rewards Score Is ', weight[np.argmax(weight)])
 
-    bandit_model = Bandit(n_arm)
-    batch_size = 100
+    bandit_model = EpsilonGreedy(n_arm)
 
-    for i in range(0, n_samples + batch_size, batch_size):
-        start = i
-        end = i + batch_size
+    batch_size = 10000
+    avg_pred_rewads = []
+    avg_rand_rewads = []
+    result_dict = {'avg_pred_rewads': [],
+                   'avg_rand_rewads': []}
+    for start in range(0, n_samples, batch_size):
+        end = np.minimum(start + batch_size, data.shape[0])
 
-        bandit_model.update()
+        batch_data = data[start:end]
+        # Bandit モデルの更新
+        bandit_model.update(data[:end])
+        # アームの選択（予測）、選ばれたアームから報酬を計算
+        selected_arm = bandit_model.select_arm(end - start)
+        is_matched = (batch_data[:, 0] == selected_arm)
+        avg_pred_reward = batch_data[is_matched, 1].mean()
+        result_dict['avg_pred_rewads'].append(avg_pred_reward)
+
+        # ランダムにアームを選択した場合の報酬を計算
+        rand_selected_arms = np.random.choice(np.arange(n_arm), size=(end - start))
+        is_matched = (batch_data[:, 0] == rand_selected_arms)
+        avg_rand_reward = batch_data[is_matched, 1].mean()
+        result_dict['avg_rand_rewads'].append(avg_rand_reward)
+
+    # 辞書型
+    plot_result(result_dict)
 
 
 if __name__ == '__main__':
