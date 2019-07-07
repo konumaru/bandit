c72c3b33963ca55103352dd3cc66529d9f74a783
louis1022 <konuma1022@gmail.com>
2019-07-08 01:54:07 +0900

    docs を作成

--- bandit.py
+++ bandit.py
@@ -1,13 +1,17 @@
 import numpy as np
-import pandas as pd
+
 
 class Bandit():
-	def __init__(self, n_arm, context=None):
-		self.n_arm = n_arm
-		self.context = context
+    def __init__(self, n_arm, context=None):
+        self.n_arm = n_arm
+        self.context = context
 
-		avg_rewards = np.zeros(n_arm)
-		self.is_context = True if context else False
+        self.avg_rewards = np.zeros(n_arm)
+        self.pulled_count = 0
+        self.is_context = True if context else False
 
-		
+    def update(self):
+        pass
 
+    def select_arm(self):
+        pass
--- main.py
+++ main.py
@@ -1,36 +1,37 @@
-import os
-
 import numpy as np
-import pandas as pd
-
 from bandit import Bandit
 
+
 def generate_data(n_arm=5, n_samples=10000):
-	np.random.seed(n_arm)
+    np.random.seed(n_arm)
 
-	n_noise = np.random.rand(n_samples)
-	weights = np.random.rand(n_arm) / 2
+    n_noise = np.random.rand(n_samples)
+    weights = np.random.rand(n_arm) / 2
 
-	pulled_arm = np.random.randint(n_arm, size=n_samples)
-	rewards = (n_noise < weights[pulled_arm]).astype(np.int8)
+    pulled_arm = np.random.randint(n_arm, size=n_samples)
+    rewards = (n_noise < weights[pulled_arm]).astype(np.int8)
 
-	return pulled_arm, rewards, weights
-
-def train_bandit():
-	pass
+    return pulled_arm, rewards, weights
 
 
 def main():
-	bandit_model = Bandit(n_arm=3)
+    bandit_model = Bandit(n_arm=3)
 
-	n_arm=5
-	n_samples=10000	
-	pulled_arm, rewards, weight = generate_data()
+    n_arm = 5
+    n_samples = 10000
+    pulled_arm, rewards, weight = generate_data(n_arm, n_samples)
 
-	train_bandit()
+    print(weight)
+
+    bandit_model = Bandit(n_arm)
+    batch_size = 100
+
+    for i in range(0, n_samples + batch_size, batch_size):
+        start = i
+        end = i + batch_size
+
+        bandit_model.update()
 
 
-
-
-if __name__=='__main__':
-	main()
+if __name__ == '__main__':
+    main()
